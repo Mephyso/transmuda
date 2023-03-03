d4219bf3449ec59b72eb38ec80294daac0ae5fcf
Kudret A <kudretnew@gmail.com>
2023-03-03 12:56:46 +0100

    US9 feature and step_defs uploaded

---
+++ src/test/java/com/transmuda/step_definitions/US9_StepDefs.java
@@ -0,0 +1,10 @@
+package com.transmuda.step_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class US9_StepDefs {
+
+    @Given("the user should see vehicle model information")
+    public void theUserShouldSeeVehicleModelInformation() {
+    }
+}
