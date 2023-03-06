bd3be91b1fe543d17507f75f25facf726255c190
cemilkutay <kutaytinmaz@gmail.com>
2023-03-06 11:30:56 +0100

    US5 changes made

--- src/test/java/com/transmuda/step_definitions/US5_StepDefs.java
+++ src/test/java/com/transmuda/step_definitions/US5_StepDefs.java
@@ -1,15 +1,23 @@
 package com.transmuda.step_definitions;
 
+import com.transmuda.pages.DashboardPage;
 import com.transmuda.pages.LoginPage;
+import com.transmuda.utilities.BrowserUtils;
+import com.transmuda.utilities.Driver;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
 
 public class US5_StepDefs {
 
-    LoginPage loginPage = new LoginPage();
-
+    DashboardPage dashboardPage = new DashboardPage();
 
     @Then("the user clicks the Vehicle Model under the Fleet")
     public void the_user_clicks_the_vehicle_model_under_the_fleet() {
+        BrowserUtils.hover(Driver.getDriver().findElement(By.xpath("//span[@class='title title-level-1'][normalize-space()='Fleet']")));
+        BrowserUtils.sleep(5);
+        dashboardPage.vehiclesModel.click();
+        BrowserUtils.sleep(5);
+
 
     }
     @Then("the user verifies managers see {int} columns in the web-table")
